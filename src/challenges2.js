b9bb8336a7779eb4bddf1f78750464773b2ca079
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-04 17:42:56 -0300

    Criei a funçao 10 techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
-}
+  let tecnologias = [];
+  let retornaVazio = 'Vazio!'
+  if (array == ''){
+    return retornaVazio;
+  }else{
+    for(let i = 0; i < array.length; i+=1){
+      array.sort();  
+      tecnologias.push(
+          { 
+            tech: array[i],
+            name: name,
+          },   
+        ); 
+      }; 
+     
+    return tecnologias;
+  }
+}  
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Lucas'))
 
 // Desafio 11
 function generatePhoneNumber() {
