3a4608bfa41738d0eddb2fbd4a483c7fab19e2b0
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-06 13:53:24 -0300

    Criei a 11 funçao generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,17 +15,52 @@ function techList(array, name) {
           },   
         ); 
       }; 
-     
     return tecnologias;
   }
 }  
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Lucas'))
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  //Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11
+  let numeroDiferente = 'Array com tamanho incorreto.'
+    if(array.length !== 11){
+    return numeroDiferente;
+  }
+
+// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números do array seja menor que 0
+// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array seja maior que 9
+  for(let inesistente = 0; inesistente < array.length; inesistente+=1){
+    let numeroMaiorOuMenor = 'não é possível gerar um número de telefone com esses valores'
+    if((array[inesistente] > 9) || (array[inesistente] < 0)){
+      return numeroMaiorOuMenor;
+    }
+  }
+
+// Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array se repetir 3 vezes ou mais
+//Para essa parte do projeto, tive ajuda do Roberval que me ajudou a entender a lógica de um for dentro de outro.
+  for(let repeticao = 0; repeticao < array.length; repeticao +=1){
+    let erroRepeticao = 'não é possível gerar um número de telefone com esses valores'
+    let numeroRepete = array[repeticao];
+    let quantidadeRepeticao = 0;
+    for(let i = 0; i < array.length; i+=1){
+      if(array[i] === numeroRepete){
+        quantidadeRepeticao +=1;
+      }
+    }
+    if(quantidadeRepeticao >= 3){
+      return erroRepeticao;
+    }
+  }
+  
+
+// Retorne um número de telefone, respeitando parênteses, traços e espaços caso os números do array estejam de acordo com as especificações
+  let ddd = '(' + array[0].toString() + array[1].toString() + ') ' ;
+  let traco = array[2].toString() + array[3].toString() + array[4].toString() + array[5].toString() + array[6].toString() + '-'  + array[7].toString() + array[8].toString() + array[9].toString() + array[10];
+
+  return ddd + traco; 
+
 }
+  
 
 // Desafio 12
 function triangleCheck() {
@@ -43,3 +78,9 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+
+
+
+
+
